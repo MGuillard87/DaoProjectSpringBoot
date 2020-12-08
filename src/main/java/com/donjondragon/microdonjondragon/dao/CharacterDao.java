3132f3ac4e5f56455439753c37d1de2f22a717ae
malika <maty.la.gui@gmail.com>
2020-12-08 13:22:14 +0100

    interface CharacterDao mis en place

---
+++ src/main/java/com/donjondragon/microdonjondragon/dao/CharacterDao.java
@@ -0,0 +1,10 @@
+package com.donjondragon.microdonjondragon.dao;
+import com.donjondragon.microdonjondragon.model.Character;
+import java.util.List;
+
+public interface CharacterDao {
+    public List<Character>findAll();
+    public Character findById(int id);
+    public Character save(Product product);
+}
+
